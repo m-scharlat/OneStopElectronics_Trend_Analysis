f451952a9ff81ed7342ceafbf7069069493a3aed
Mayer <mayer.scharlat@gmail.com>
2024-11-01 16:41:28 -0400

    Update advanced_queries.sql

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -1,4 +1,4 @@
-## OneStopElectronics: Additional Stakeholder Question - SQL Queries
+## OneStopElectronics: Additional Stakeholder Questions - Advanced SQL Queries
 
 ## (1) What were the order counts, sales, and AOV for Macbooks sold in North America for each quarter across all years? 
 
