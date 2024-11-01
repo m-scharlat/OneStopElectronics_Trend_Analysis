2dfa43adb9dc5f1bfd36de93d007670232568576
Mayer <mayer.scharlat@gmail.com>
2024-11-01 16:39:53 -0400

    Update and rename OneStopElectronics_queries.sql to Additional_Stakeholder_Questions_queries.sql

--- Additional_Stakeholder_Questions_queries.sql
+++ Additional_Stakeholder_Questions_queries.sql
@@ -1,4 +1,4 @@
-## OneStopElectronics - SQL Queries
+## OneStopElectronics: Additional Stakeholder Question - SQL Queries
 
 ## (1) What were the order counts, sales, and AOV for Macbooks sold in North America for each quarter across all years? 
 
